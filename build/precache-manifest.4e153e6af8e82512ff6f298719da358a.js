82b7dfde76bc7141a9409e5f7ec88f5ea31ae0ac
bhavikbhoir <bhvkbhoir95@gmail.com>
2020-06-12 22:02:24 -0400

    React-trial

---
+++ build/precache-manifest.4e153e6af8e82512ff6f298719da358a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1aa4811e21a30cc6db435b57781be6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1440ff9503939833e33",
+    "url": "/static/css/main.e75b8e03.chunk.css"
+  },
+  {
+    "revision": "f66ff9d58e681ad3955f",
+    "url": "/static/js/2.cc9dbef5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc9dbef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1440ff9503939833e33",
+    "url": "/static/js/main.6288ef2a.chunk.js"
+  },
+  {
+    "revision": "e3d13cafbac29239fc1b",
+    "url": "/static/js/runtime-main.f50e1c7a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
